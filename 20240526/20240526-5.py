0146b8e89167f87974da6c7b9fd6116d0a2ba117
龔宇謙 <ygong076@gmail.com>
2024-05-26 06:58:07 +0000

    6

---
+++ 20240526/20240526-5.py
@@ -0,0 +1,34 @@
+from random import *
+from pyinputplus import *
+
+def play_game():
+    min:int = 1
+    max:int = 100
+    target:int = randint(min, max) #x:int  type.hint(不強制轉型)
+    count:int = 0
+
+    print("===========猜數字遊戲===========\n")
+    while True:
+        count += 1
+        keyin:int = inputInt(f"猜數字(範圍{min}~{max}): ", min=min, max=max)
+        print(keyin)
+        if keyin == target:
+            print(f"答對了~答案是{keyin}")
+            print(f"你共猜了 {count} 次")
+            break
+        elif keyin > target:
+            print("再小一點~")
+            max = keyin - 1
+            print(f"你已經猜了 {count} 次")
+        else:
+            print("再大一點~")
+            min = keyin + 1
+            print(f"你已經猜了 {count} 次")
+
+
+while True:
+    play_game()
+    playagain:str = inputYesNo("請問還要繼續嗎?(y, n)")
+    print()
+    if playagain == "no":
+        break
